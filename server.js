f15a15cec7d49b5477bbfd27c39e5f962e65479a
Ly Dang <lydang26993@gmail.com>
2024-03-03 10:41:03 -0700

    Initial commit for project 1 lesson 1

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-const express = require( 'express' );
+// W01 Learning Activity: Web Services and Node Architecture
+// const express = require( 'express' );
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Hello Ly');
+// });
+
+// const port = 3000;
+
+// app.listen(process.env.port || port);
+// console.log('Web Server is listening at port' + (process.env.port || port));
+
+
+
+
+
+
+// Contacts Project: Part 1 START HERE
+
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Ly');
-});
+// Import Routes
+app.use('/', require('./routes'));
 
-const port = 3000;
-
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port' + (process.env.port || port));
+// Start the server
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+  });

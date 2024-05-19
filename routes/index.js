b8afed5b53bccb1a9a9483db8ddb44eeef06f84c
lillyasquith <asq22001@byui.edu>
2024-05-18 22:49:43 -0600

    W04PersonalAssignment completed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
-const router = require ('express').Router()
+const router = require ('express').Router();
+router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {res.send('Hello!')})
+router.get('/', (req, res) => {res.send('Hello!')});
 
 router.use('/contacts', require('./contacts'));
 
